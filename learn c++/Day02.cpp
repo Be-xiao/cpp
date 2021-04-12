2e6e4d7aa4090bbc37e349b4a42ead5b929cafd3
Be-xiao <lessonxiao@qq.com>
2021-04-12 02:12:10 -0400

    master

--- learn c++/Day02.cpp
+++ learn c++/Day02.cpp
@@ -47,3 +47,21 @@ int func()
 }
 */
 
+// 定义常量
+#define identifier value
+
+#include<iostream>
+using namespace std;
+
+#define LENGTH 10
+#define WIDTH 5
+#define NEWLINE '\n'
+
+int main()
+{
+    int area;
+    area = LENGTH*WIDTH;
+    cout << area;
+    cout << NEWLINE;
+    return 0;
+}
